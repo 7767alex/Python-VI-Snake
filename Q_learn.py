2a2a9f9a31e485aa442e051670669c9b4a299530
tquigg96 <62739843+tquigg96@users.noreply.github.com>
2020-12-04 14:23:14 -0800

    Update Q_learn.py

--- Q_learn.py
+++ Q_learn.py
@@ -3,49 +3,64 @@ import numpy as np
 def env(params):
 
     global action, rewards, moveableArea, q_values
-
+    rows = 2 + params['screenSizeX'] / 10
+    columns = 2 + params['screenSizeY'] / 10
     action = ["UP", "DOWN", "LEFT", "RIGHT"]
-    q_values = np.zeros((params['screenSizeX'], params['screenSizeY'], 3))
-    rewards = np.full([params['screenSizeX'], params['screenSizeY']], -100)
-    rewards[int(params['food_posx']), int(params['food_posy'])] = 5
-    # print(rewards)
-    moveableArea = {}
+    q_values = np.zeros((int(2 + params['screenSizeX']/10), int(2+params['screenSizeY'] / 10), 4))
 
-    for i in range(1, params['screenSizeY']):
-        moveableArea[i] = [i for i in range(1, params['screenSizeX']-1)]  # come back to this
+    rewards = np.full([int(2+params['screenSizeX']/10), int(2+params['screenSizeY']/10)], -1)
+    rewards[int(2+params['food_posx']/10), int(2+params['food_posy']/10)] = 500
+    rewards[int(2+params['snake_pos'][0]/10), int(2+params['snake_pos'][1]/10)] = 50
 
-    for row_index in range(1, params['screenSizeX']-1):
-        for column_index in moveableArea[row_index]:
-            rewards[row_index, column_index] = 1
-    print(rewards)
+
+
+    for i in range(int(columns)):
+        rewards[0][int(i) - 1] = -100
+        rewards[int(rows) - 1][int(i) - 1] = -100
+
+    for i in range(int(rows)):
+        rewards[int(i)][0] = -100
+        rewards[int(i)][int(columns) - 1] = -100
+    #print(rewards)
+
     return q_values
 
 def is_terminal_state(current_row_index, current_column_index):
-    if rewards[current_row_index, current_column_index] == 1:
+    #print(current_column_index)
+    #print(current_row_index)
+    if rewards[int(2+current_row_index/10), int(2+current_column_index/10)] == -1 or rewards[int(2+current_row_index/10), int(2+current_column_index/10)] == 5:
+        #print(rewards)
         return False
-    else:
+    elif rewards[int(2+current_row_index/10), int(2+current_column_index/10)] == -100:
+        #print(rewards)
         return True
 
-def get_starting_location(params, e):
-    current_row_index = params['snake_pos'][0]
-    current_column_index = params['snake_pos'][1]
+def get_starting_location(params):
+    current_row_index = params['snake_pos'][0] / 10
+    current_column_index = params['snake_pos'][1] / 10
+    #print(current_row_index + current_column_index)
     while is_terminal_state(current_row_index, current_column_index):
-        Move = np.random.choice([True, False], p=[1 - e, e])
-    return Move
+        current_row_index = params['snake_pos'][0] / 10
+        current_column_index = params['snake_pos'][1] / 10
+    return current_row_index, current_column_index
 
-def get_next_action(params, current_row_index, current_column_index, e):
-    moveBool = get_starting_location(params, e)
-    if moveBool == False:
-        return np.argmax(q_values[current_row_index, current_column_index])
+def get_next_action(params, current_row_index, current_column_index,epsilon):
+
+
+
+    if np.random.random() < epsilon:
+        return np.argmax(q_values[int(2+current_row_index/10), int(2+current_column_index/10)])
     else:
         return True
+
 
 
 def get_next_location(params, current_row_index, current_column_index, action_index):
     new_row_i = current_row_index
     new_col_i = current_column_index
+
     if action[action_index] == 'UP' and current_row_index > 0:
-        new_row_i = 1
+        new_row_i -= 1
     elif action[action_index] == "RIGHT" and current_row_index < params['screenSizeX'] - 1:
         new_col_i += 1
     elif action[action_index] == "DOWN" and current_row_index < params['screenSizeY'] - 1:
@@ -65,31 +80,45 @@ def get_shortest_path(params, start_row_index, start_column_index):
 
         while not is_terminal_state(current_row_index, current_column_index):
             action_index = get_next_action(params, current_row_index, current_column_index, 1)
-            current_row_index, current_column_index = get_next_location(current_row_index, current_column_index,
+            current_row_index, current_column_index = get_next_location(params, current_row_index, current_column_index,
                                                                         action_index)
             shortest_path.append([current_row_index, current_column_index])
-        return shortest_path
 
-def Q_train(q_values, epsilon, gamma, learning_rate):
-    for episode in range(1000):
-        row_index, column_index = get_starting_location()
-        q_values = [row_index, column_index, action]
+        return action_index
 
-    while not is_terminal_state(row_index, column_index):
-        action_check = get_next_action(row_index, column_index, epsilon)
-        
-        if(action_check == True):
-            action_index = np.random.randint(4)
-        else:
-            action_index = action_check
+def Q_train(params, epsilon, gamma, learning_rate):
+    for episode in range(1):
+        row_index, column_index = get_starting_location(params)
+        row_index = 2 + row_index
+        column_index = 2 + column_index
+        #print(row_index)
+        #print(column_index)
+
+        while not is_terminal_state(row_index, column_index):
+
+            action_check = get_next_action(params, row_index, column_index, epsilon)
+
+            #print(action_check)
+            #print(column_index)
+            if(action_check == True):
+                action_index = np.random.randint(3)
+            else:
+                action_index = action_check
 
 
-        old_row_index, old_column_index = row_index, column_index
-        row_index, column_index = get_next_location(row_index, column_index, action_index)
+            old_row_index, old_column_index = row_index, column_index
+            #print(get_next_location(params, row_index, column_index, action_index))
+            row_index, column_index = get_next_location(params, row_index, column_index, action_index)
+            #print(get_next_location(params, row_index, column_index, action_index))
 
-        reward = rewards[row_index, column_index]
-        old_q_value = q_values[old_row_index, old_column_index, action_index]
-        temporal_difference = reward + (gamma * np.max(q_values[row_index, column_index])) - old_q_value
+            reward = rewards[int(row_index), int(column_index)]
+            old_q_value = q_values[int(old_row_index), int(old_column_index), int(action_index)]
+            temporal_difference = reward + (gamma * np.max(q_values[2])) - old_q_value
 
-        new_q_value = old_q_value + (learning_rate * temporal_difference)
-        q_values[old_row_index, old_column_index, action_index] = new_q_value
+            new_q_value = old_q_value + (learning_rate * temporal_difference)
+            q_values[int(old_row_index), int(old_column_index)] = new_q_value
+            print(q_values)
+            #print(q_values)
+
+print("training complete")
+            #print(q_values)
