2995d978e6a037a23b23bbc60e5a36dd032ae2bb
tquigg96 <62739843+tquigg96@users.noreply.github.com>
2020-12-04 19:18:52 -0800

    Update Q_learn.py

--- Q_learn.py
+++ Q_learn.py
@@ -22,31 +22,34 @@ def env(params):
     for i in range(int(rows)):
         rewards[int(i)][0] = -100
         rewards[int(i)][int(columns) - 1] = -100
-    print(rewards)
+    #print(rewards)
 
     return q_values
 
-def is_terminal_state(current_row_index, current_column_index):
+def is_terminal_state(params, current_row_index, current_column_index):
     #print(current_column_index)
     #print(current_row_index)
     if rewards[int(2+current_row_index/10), int(2+current_column_index/10)] == -1 or rewards[int(2+current_row_index/10), int(2+current_column_index/10)] == 5:
        # print(rewards)
-        print(rewards[int(2+current_row_index/10), int(2+current_column_index/10)])
+       #print(rewards[int(2+current_row_index/10), int(2+current_column_index/10)])
        # print(current_column_index)
        # print(current_row_index)
         return False
-    elif rewards[2 + int(current_row_index/10), 2 + int(current_column_index/10)] == -100:
+    elif (2 + params['snake_pos'][0] / 10) >= (2 + params['screenSizeX'] / 10) or (2 + params['snake_pos'][1] / 10) >= (2 + params['screenSizeY'] / 10):
         #print(current_column_index)
         #print(current_row_index)
         return True
 
 def get_starting_location(params):
-    current_row_index = params['snake_pos'][0] / 10
-    current_column_index = params['snake_pos'][1] / 10
+    params['snake_pos'][0] = 50
+    params['snake_pos'][1] = 50
+    current_row_index = 2+ params['snake_pos'][0] / 10
+
+    current_column_index = 2+ params['snake_pos'][1] / 10
     #print(current_row_index + current_column_index)
-    while is_terminal_state(current_row_index, current_column_index):
-        current_row_index = params['snake_pos'][0] / 10
-        current_column_index = params['snake_pos'][1] / 10
+    while is_terminal_state(params, current_row_index, current_column_index):
+        current_row_index = 2+ params['snake_pos'][0] / 10
+        current_column_index = 2+ params['snake_pos'][1] / 10
     return current_row_index, current_column_index
 
 def get_next_action(params, current_row_index, current_column_index,epsilon):
@@ -54,11 +57,11 @@ def get_next_action(params, current_row_index, current_column_index,epsilon):
 
 
     if np.random.random() < epsilon:
-        print("max" + str(np.argmax(q_values[int(2 + current_row_index / 10), int(2 + current_column_index / 10)])))
+        #print("max" + str(np.argmax(q_values[int(2 + current_row_index / 10), int(2 + current_column_index / 10)])))
         return np.argmax(q_values[int(2+current_row_index/10), int(2+current_column_index/10)])
 
     else:
-        print("rand" + str(np.random.randint(4)))
+        #print("rand" + str(np.random.randint(4)))
         return np.random.randint(4)
 
 
@@ -89,14 +92,14 @@ def get_shortest_path(params, start_row_index, start_column_index):
     global action_index
     print(rewards)
     print("hello World")
-    if is_terminal_state(start_row_index, start_column_index):
+    if is_terminal_state(params, start_row_index, start_column_index):
         return []
     else:
         current_row_index, current_column_index = start_row_index, start_column_index
         shortest_path = []
         shortest_path.append([current_row_index, current_column_index])
 
-        while not is_terminal_state(current_row_index, current_column_index):
+        while not is_terminal_state(params, current_row_index, current_column_index):
             action_index = get_next_action(params, current_row_index, current_column_index, 1)
             current_row_index, current_column_index = get_next_location(params, current_row_index, current_column_index,
                                                                         action_index)
@@ -108,11 +111,12 @@ def Q_learnAlg(params, row_index, column_index):
         reward = -100
         return reward
     else:
-        reward = rewards[int(row_index - 1), int(column_index)]
+        reward = rewards[int(row_index - 1), int(column_index-1)]
         return reward
 
 def Q_train(params, epsilon, gamma, learning_rate):
-    for episode in range(100000000000000000):
+    victory = 0
+    for episode in range(1000):
         row_index, column_index = get_starting_location(params)
         row_index = row_index
         column_index = column_index
@@ -120,7 +124,7 @@ def Q_train(params, epsilon, gamma, learning_rate):
         #print(row_index)
         #print(column_index)
         #print(row_index, column_index)
-        while not is_terminal_state(row_index, column_index):
+        while not is_terminal_state(params, row_index, column_index):
             #print("in while loop")
             #print(is_terminal_state(row_index, column_index))
             action_index = get_next_action(params, row_index, column_index, epsilon)
@@ -140,12 +144,21 @@ def Q_train(params, epsilon, gamma, learning_rate):
 
             new_q_value = old_q_value + (learning_rate * temporal_difference)
             q_values[int(old_row_index), int(old_column_index)] = new_q_value
-            iteration = 1
+
+
+
+            if (int(2+params['snake_pos'][0]/10) == int(2+params['food_posx']/10) and int(2+params['snake_pos'][1]/10) == int(2+params['food_posy']/10)):
+                victory += 1
+                print("victory!!!")
+    print("TimesFoodEaten: " + str(victory))
+
+
 
             #print("iteration: " + str(iteration))
             #print(q_values)
-            iteration += iteration
+
         #print("Q_values: " + str(q_values))
         #print("done")
 
     print("training complete")
+            #print(q_values)
